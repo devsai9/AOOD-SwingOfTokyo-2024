166712ba1db5ff5f9e19a6d48d961fdccf51b9f4
devsai9 <63507117+devsai9@users.noreply.github.com>
2024-10-30 18:16:27 -0400

    More basic structure

--- src/KOT_Swing.java
+++ src/KOT_Swing.java
@@ -8,6 +8,21 @@ public class KOT_Swing {
     JLabel background;
 
     public KOT_Swing() {
+        frame = new JFrame("King of Tokyo GUI");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(800, 600);
+
+        panel = new JPanel();
+        panel.setBounds(0, 0, 800, 600);
+        panel.setBackground(Color.WHITE);
+        panel.setLayout(new GridLayout(1, 3));
+
+        background = new JLabel();
+        background.setBackground(Color.BLACK);
+
+        frame.add(panel);
+
+        frame.setVisible(true);
 
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        GameEngine ge = new GameEngine();
+        // GameEngine ge = new GameEngine();
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                KOT_Swing k = new KOT_Swing();
+            }
+        });
     }
 }
